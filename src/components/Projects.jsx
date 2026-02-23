dceaa63c9d15c6f9a411cc30b274526c0fb54b51
nathanielpierson <nathanielepierson@gmail.com>
2026-02-23 17:06:07 -0500

    Add introductory subtitle to Projects component and update CSS for styling

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -11,6 +11,9 @@ function Projects() {
           Here are some of my recent projects, representing a variety of
           accomplishments and technologies I've learned and mastered.
         </p>
+        <p className="section-subtitle projects-intro">
+          Click Learn More to see detailed explanations of my projects, samples of code, demo videos of me using the project, and more!
+        </p>
         <div className="projects-grid">
           {projects.map((project, index) => (
             <ProjectCard key={index} project={project} />
